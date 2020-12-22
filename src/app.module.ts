0e08738ab18446964831b989553976ff45b3dcad
Rémi <6328789+Marchelune@users.noreply.github.com>
2020-12-23 00:00:16 +0100

    Register in memory mongo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ForestModel, ForestSchema } from './db-models';
 import { ForestController } from './forest/forest.controller';
+import { InMemoryMongoService } from './in-memory-mongo/in-memory-mongo.service';
+import { InMemoryMongoModule } from './in-memory-mongo/in-memory-mongo.module';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: ForestModel.name, schema: ForestSchema }])],
+    imports: [
+        MongooseModule.forFeature([{ name: ForestModel.name, schema: ForestSchema }]),
+        MongooseModule.forRootAsync({
+            useFactory: async (mongoInMemory: InMemoryMongoService) => {
+                const connString = await mongoInMemory.getMongoConnString();
+                return {
+                    uri: connString,
+                };
+            },
+            inject: [InMemoryMongoService],
+            imports: [InMemoryMongoModule],
+        }),
+    ],
     controllers: [ForestController],
-    providers: [],
+    providers: [InMemoryMongoService],
 })
 export class AppModule {}
